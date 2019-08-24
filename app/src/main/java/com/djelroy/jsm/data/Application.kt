c9fbba86d7247c1872673cf32d497b140d109757
DJ Elroy <djelroys@gmail.com>
2019-08-23 20:07:33 -0400

    Convert into an Entity

--- app/src/main/java/com/djelroy/jsm/data/Application.kt
+++ app/src/main/java/com/djelroy/jsm/data/Application.kt
@@ -1,3 +1,13 @@
 package com.djelroy.jsm.data;
 
-data class Application(val company: String, val title: String, val description: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.annotation.NonNull
+import java.util.*
+
+@Entity(tableName = "applications", primaryKeys = ["company", "title"])
+class Application(@ColumnInfo(name = "company") val company: String ,
+                  @ColumnInfo(name = "title") val title: String ,
+                  @ColumnInfo(name = "description") val description: String/*,
+                  @ColumnInfo(name = "submitted_date") val submittedDate: Date = Date()*/)
